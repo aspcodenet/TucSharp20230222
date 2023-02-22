278b8982a1d14a3c7ab73dec21b526ba6f71fdab
aspcodenet <stefan.holmberg@systementor.se>
2023-02-22 09:46:33 +0100

    Add project files.

---
+++ TucSharp20230222/App.cs
@@ -0,0 +1,52 @@
+﻿public class App
+{
+    public void Run()
+    {
+        // KONTO
+        // många 
+        var kontoList = new List<Konto>();
+        
+        var a = new Konto();
+        a.AccountNo = "456";
+        a.Balance = 100;
+        kontoList.Add(a);
+
+        kontoList.Add(new Konto{ AccountNo = "123", Balance = 1200});
+
+
+        while (true)
+        {
+            Console.WriteLine("1. Withdraw");
+            Console.WriteLine("2. Deposit");
+            Console.WriteLine("3. Print all accounts");
+            int sel = Convert.ToInt32(Console.ReadLine());
+            if(sel == 3)
+            {
+                foreach (var acc in kontoList)
+                {
+                    Console.WriteLine($"{acc.AccountNo} {acc.Balance} kr");
+                }
+            }
+            if (sel == 1)
+            {
+                Console.WriteLine("Ange kontonummer:");
+                var kontoNr = Console.ReadLine();
+
+                Console.WriteLine("Ange belopp:");
+                var belopp = Convert.ToInt32(Console.ReadLine());
+
+                //var konto = Hitta konto med Kontonnummer = kontoNr
+            }
+
+        }
+    }
+
+
+}
+
+public class Konto
+{
+    public int Balance{ get; set; }
+    public string AccountNo{ get; set; }
+
+}
---
+++ TucSharp20230222/AtmMachine.cs
@@ -0,0 +1,36 @@
+﻿public class AtmMachine
+{
+
+    public Dictionary<string,int> konton = new Dictionary<string,int>();
+
+    public AtmWithdrawalStatus Withdraw(string accountNumber, int belopp)
+    {
+        if (!konton.ContainsKey(accountNumber))
+            return AtmWithdrawalStatus.AccountMissing;
+        if (belopp > 3000) 
+            return AtmWithdrawalStatus.MoreThanMaxWithdrawal;
+        if(belopp > konton[accountNumber])
+            return AtmWithdrawalStatus.NotEnoughBalance;
+        
+        konton[accountNumber] -= belopp;
+        
+        return AtmWithdrawalStatus.Ok;
+    }
+
+    public int GetBalance(string accountNumber)
+    {
+        return konton[accountNumber];
+    }  // => TryGetBalance
+
+        
+}
+
+public enum AtmWithdrawalStatus
+{
+    Ok,
+    AccountMissing,
+    MoreThanMaxWithdrawal,
+    //Max per dag?
+    NotEnoughBalance
+
+}
